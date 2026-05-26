215ac0b8b59e99ac9b4e8ad4b8e15d590d070d4e
practice_oneroom-admin <i.oneroom.bn@gmail.com>
2026-05-26 11:20:34 +0900

    投稿画面の作成、font-awesomeの利用、css変更

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root to: 'articles#index'
 
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :new, :create]
   # root "posts#index"e
 end
