418a796edb12ec9d5db7a6e4ad929aee068e418f
practice_oneroom-admin <i.oneroom.bn@gmail.com>
2026-05-26 17:27:37 +0900

    記事を削除できるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root to: 'articles#index'
 
-  resources :articles, only: [:show, :new, :create, :edit, :update]
+  resources :articles, only: [:show, :new, :create, :edit, :update, :destroy]
   # root "posts#index"e
 end
