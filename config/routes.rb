e051e13d3eaea7d24ffc8013ea1e1fa82f25dda4
Atsushi-iwaguchi <i.oneroom.bn@gmail.com>
2026-06-01 15:32:45 +0900

    like記事一覧表示

--- config/routes.rb
+++ config/routes.rb
@@ -16,9 +16,10 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments, only: [ :new, :create ]
 
-    resource :like, only: [:create, :destroy]
+    resource :like, only: [ :create, :destroy ]
   end
   # root "posts#index"e
 
   resource :profile, only: [ :show, :edit, :update ]
+  resources :favorites, only: [ :index ]
 end
